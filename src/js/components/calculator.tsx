e4502169d0c59aaa5fdf160101fcd2b3c4f4775f
Sergey <dushechkin_serge@mail.ru>
2021-03-29 17:58:31 +0300

    Inserts components

--- src/js/components/calculator.tsx
+++ src/js/components/calculator.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 import {CarCalculator} from "../types";
 import {formatFromMonth, formatPrice, formatDummy} from "../utils";
+
 import Slider from "./slider";
+import Results from "./results";
+import Indicator from "./indicator";
 
 interface Props {
   name: string;
@@ -16,79 +19,19 @@ export const Calculator:React.FunctionComponent<Props> = (props: Props) => {
     <div className="car__calculator calculator">
       <ul className="calculator__fields fields">
         <li className="fields__field field">
-          <div className="field__indicator indicator">
-            <div className="indicator__description">Цена {name}:</div>
-            <div className="indicator__values">
-              <div className="indicator__value">{formatPrice(price.min)} <span className="indicator__currency">&#8381;</span></div>
-            </div>
-          </div>
-          <Slider min={price.min} max={price.max} selected={price.min} measure={`\u20bd`} cbFormat={formatPrice} />
-          <div className="field__slider slider">
-            <div className="slider__axis">
-              <div className="slider__cursor"><span className="visually-hidden">Control</span></div>
-            </div>
-            <div className="slider__labels">
-              <div className="slider__min">{formatPrice(price.min)} &#8381;</div>
-              <div className="slider__max">{formatPrice(price.max)} &#8381;</div>
-            </div>
-          </div>
+          <Indicator title={`Цена ${name}:`} value={formatPrice(price.selected)} currency={`\u20bd`} percent={``}/>
+          <Slider min={price.min} max={price.max} selected={price.selected} measure={`\u20bd`} cbFormat={formatPrice} />
         </li>
         <li className="fields__field field">
-          <div className="field__indicator indicator">
-            <div className="indicator__description">Первоначальный взнос</div>
-            <div className="indicator__values">
-              <div className="indicator__value">{formatPrice(price.min / 100 * contribution.min)} <span className="indicator__currency">&#8381;</span></div>
-              <div className="indicator__percent">{contribution.min}%</div>
-            </div>
-          </div>
-          <div className="field__slider slider">
-            <div className="slider__axis">
-              <div className="slider__cursor"><span className="visually-hidden">Control</span></div>
-            </div>
-            <div className="slider__labels">
-              <div className="slider__min">{contribution.min}%</div>
-              <div className="slider__max">{contribution.max}%</div>
-            </div>
-          </div>
+          <Indicator title={`Первоначальный взнос`} value={formatPrice(price.min / 100 * contribution.min)} currency={`\u20bd`} percent={`${contribution.min}`}/>
+          <Slider min={contribution.min} max={contribution.max} selected={contribution.selected} measure={`%`} cbFormat={formatDummy} />
         </li>
         <li className="fields__field field">
-          <div className="field__indicator indicator">
-            <div className="indicator__description">Срок лизинга</div>
-            <div className="indicator__values">
-              <div className="indicator__value">{formatFromMonth(leasing.selected)}</div>
-            </div>
-          </div>
-          <div className="field__slider slider">
-            <div className="slider__axis">
-              <div className="slider__cursor"><span className="visually-hidden">Control</span></div>
-            </div>
-            <div className="slider__labels">
-              <div className="slider__min">{formatFromMonth(leasing.min)}</div>
-              <div className="slider__max">{formatFromMonth(leasing.max)}</div>
-            </div>
-          </div>
+          <Indicator title={`Срок лизинга`} value={formatFromMonth(leasing.selected)} currency={``} percent={``}/>
+          <Slider min={leasing.min} max={leasing.max} selected={leasing.selected} measure={``} cbFormat={formatFromMonth} />
         </li>
       </ul>
-      <div className="calculator__results results">
-        <div className="results__main">
-          <div className="results__text">
-            <div className="results__description">Налоговая экономия на юр.лиц до</div>
-            <div className="results__price">
-              {formatPrice(economy)} <span className="results__currency">&#8381;</span>
-            </div>
-          </div>
-          <div className="results__button results__button--closed"><span className="visually-hidden">Open</span></div>
-        </div>
-        <div className="results__details">
-          <div className="results__description">Ежемесячный платеж от</div>
-          <div className="results__text">
-            <div className="results__price results__price--month">
-              {formatPrice(payment.month)} <span className="results__currency results__currency--month">&#8381;</span>
-              <span className="results__day">или {formatPrice(payment.day)} &#8381; в день</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Results payment={payment} economy={economy} />
       <div className="calculator__annotation">Расчет носит предварительный характер</div>
     </div>
   );
--- src/js/components/slider.tsx
+++ src/js/components/slider.tsx
@@ -10,13 +10,48 @@ interface Props {
 
 export const Slider:React.FunctionComponent<Props> = (props: Props) => {
   const {min, max, selected, measure, cbFormat} = props;
-  console.log(`${(min / max) * selected}%`);
-  const position = {left: `${min / max * selected}%`};
+
+  const cursorRef = React.useRef(null);
+
+  const percent = Math.ceil(100 * (selected - min) / (max - min));
+  const offset = percent > 5 ? 10 : 0;
+  const position = {left: `calc(${percent}% - ${offset}px)`};
+
+  const mouseDownHandle = (downEvt) => {
+    downEvt.persist();
+    let startX = downEvt.clientX;
+    console.log(cursorRef);
+    console.log(downEvt);
+
+    const MouseUpHandler = (upEvt) => {
+      console.log(`upEvt: ${upEvt}`);
+
+      document.removeEventListener(`mouseup`, MouseUpHandler);
+      document.removeEventListener(`mousemove`, MouseMoveHandler);
+    };
+
+    const MouseMoveHandler = (moveEvt) => {
+      console.log(`moveEvt: ${moveEvt}`);
+
+      const shiftX = startX - moveEvt.clientX;
+      startX = moveEvt.clientX;
+      const offsetX = cursorRef.current.offsetLeft - shiftX;
+
+      cursorRef.current.innerText = `${startX} ${shiftX} ${offsetX}`;
+
+      cursorRef.current.style.left = `${100}%`;
+    };
+
+    console.log(`donwEvt: ${downEvt}`);
+
+    document.addEventListener(`mouseup`, MouseUpHandler);
+    document.addEventListener(`mousemove`, MouseMoveHandler);
+  };
 
   return (
     <div className="field__slider slider">
       <div className="slider__axis">
-        <div className="slider__cursor" style={position}><span className="visually-hidden">Control</span></div>
+        <div ref={cursorRef} onMouseDown={mouseDownHandle} className="slider__cursor" style={position}><span className="visually-hidden">Control</span></div>
       </div>
       <div className="slider__labels">
         <div className="slider__min">{cbFormat(min)}{measure}</div>
